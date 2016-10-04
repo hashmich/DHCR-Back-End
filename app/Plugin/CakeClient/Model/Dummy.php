e554bc7825749534de8ab7a299246600c3db247b
Hendrik Schmeer <mail@hendrikschmeer.de>
2016-10-04 13:47:14 +0200

    ...
    ...

--- app/Plugin/CakeClient/Model/Dummy.php
+++
@@ -1,9 +0,0 @@
-<?php
-// we need to create this, to make the plugin's AppModel
-// methods available without loading any other model
-class Dummy extends CakeclientAppModel {
-	
-	public $useTable = false;
-	
-}
-?>
