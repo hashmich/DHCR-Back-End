7716be4298babae7abe56d15e6882be970bacb8e
hashmich <mail@hendrikschmeer.de>
2017-06-29 11:14:55 +0200

    updated test task to replicate checkUrl task

--- app/Console/Command/Task/TestTask.php
+++ app/Console/Command/Task/TestTask.php
@@ -2,9 +2,51 @@
 
 class TestTask extends Shell {
 	
-	public $uses = array();
+	public $uses = array('Course');
 	
 	
+	public function execute() {
+		$collection = $this->Course->checkUrls();
+		if(!empty($collection)) {
+			
+			if(1) {
+				App::uses('CakeEmail', 'Network/Email');
+	
+				foreach($collection as $email => $data) {
+					if($email == 'no_owner') continue;
+						
+					$Email = new CakeEmail('default');
+					$subject_prefix = (Configure::read('App.EmailSubjectPrefix'))
+					? trim(Configure::read('App.EmailSubjectPrefix')) . ' '
+							: '';
+								
+					$options = array(
+							'subject_prefix' => $subject_prefix,
+							'subject' => 'Invalid URLs',
+							'emailFormat' => 'text',
+							'template' => 'invalid_urls',
+							'layout' => 'default',
+							'email' => $email,
+							'data' => $data
+					);
+					if(is_string($options['email'])) {
+						$Email->to('mail@hendrikschmeer.de');
+						$Email->emailFormat($options['emailFormat']);
+						$Email->subject($options['subject_prefix'] . $options['subject']);
+						$Email->template($options['template'], $options['layout']);
+						$Email->viewVars(array(
+								'data' => $options['data']
+						));
+						
+						$Email->send();
+					}
+					unset($Email);
+				}
+			}
+		}
+	}
+	
+	/*
 	public function execute($to = null) {
 		$this->out("Executing TestTask...");
 		
@@ -26,6 +68,7 @@ class TestTask extends Shell {
 			$Email->send('If you recieve this mail, the CronShell successfully executed task "Test".');
 		}
 	}
+	*/
 	
 }
 ?>
