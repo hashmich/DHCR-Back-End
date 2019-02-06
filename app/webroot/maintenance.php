d33584425b536a3ee3f7a5958c37ea71c0acd603
Hendrik Schmeer <mail@hendrikschmeer.de>
2019-02-06 15:17:22 +0100

    fixes #19 maintenance http header

--- app/webroot/maintenance.php
+++ app/webroot/maintenance.php
@@ -1,3 +1,6 @@
+
+<?php header($_SERVER["SERVER_PROTOCOL"].' 503 Service Unavailable'); ?>
+
 <h1>Maintenance</h1>
 <p>
 	Due to maintenance work, the DH-Courseregistry will be unavailable for a short time.<br>
